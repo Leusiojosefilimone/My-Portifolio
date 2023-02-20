e01e35ebcc79f5d41399c0a4a42ec0b7c6066f67
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-20 14:43:55 -0300

    first comit
    i create the function escrever which is write a description text on profile

---
+++ main.js
@@ -0,0 +1,33 @@
+const title = document.querySelector('.write')
+const text  =title.innerHTML
+var index = 0
+
+
+const escrever = () => {
+    title.innerHTML = title.innerHTML.replace('|', '')
+    if(text.length > index){
+        if(index === 0){
+            title.innerHTML = text.charAt(index)
+        }else{
+            title.innerHTML+= text.charAt(index)
+        }
+        title.innerHTML += '|'
+    }
+    index++
+    setTimeout(escrever, 50)
+}
+escrever()
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: "swiper-pagination"
+    },
+    mausewheel: true,
+    keyboard: true
+})
